0747c46f37e226d9c299160c997787effced35e4
Meridian <mwitt@wellesley.edu>
2015-05-06 11:49:29 -0400

    Final Version of Code
    Fixed for deployment - removed Google Accounts

--- mwitt_am6.js
+++ mwitt_am6.js
@@ -1,163 +1,79 @@
-//Projection Id essential-storm-93200
-PlayersList = new Mongo.Collection('players');
-// console.log("Hello world");
-
-if(Meteor.isServer){
- 
-    
-    /*Eni's code from meeting app*/
-    
-    Meetings = new Mongo.Collection('meetings');
-
-/* Helper function used in the publish method to filter undesired items.*/
-function checkExpiration(){
-  // Iterate through all meetings and check if time has expired
-  var allMeetings = Meetings.find({});
-  var now = new Date();
-  allMeetings.forEach(function(item){
-    if (item.endTimeDate < now)
-      Meetings.update(item, {$set: {hasExpired: true}});
-  })
-  // remove expired entries
-  Meetings.remove({hasExpired: true})
-}
-
-
-Meteor.publish('theMeetings', function(){
-    checkExpiration();
-    return Meetings.find({hasExpired: false}, {sort: {startTimeDate: 1}})
-});
-
-
-Meteor.methods({
-  insertMeeting: function(meetingObj){
-    // create two new fields for startTime and endTime, so that they are
-    // Date() objects
-    var startTimeObj = new Date(meetingObj.date + " " + meetingObj.startTime);
-    var endTimeObj = new Date(meetingObj.date + " " + meetingObj.endTime);
-//    var currentUserId = Meteor.user().username;
-      
-    Meetings.insert({
-      name: currentUserId,
-      location: meetingObj.location,
-      dateStr: meetingObj.date,
-      // Use the moment.js library to format time in US time
-      startTimeStr: moment(startTimeObj).format("hh:mm A"),
-      endTimeStr: moment(endTimeObj).format("hh:mm A"),
-      startTimeDate: startTimeObj,
-      endTimeDate: endTimeObj,
-        activityStr: meetingObj.activity,
-      hasExpired: false
-    })
-    console.log("new meeting added in mongodb");
-  }
-})
-
-}
+/*
+Team: Chimuanya Okoro, Sophia Zachares, Meridian Witt
+File: mwitt_am6.js
+Purpose: The client has the template location and 
+Note: Events is the terminology held over from Eni's example application. We are creating events. 
+*/
 
 
 if(Meteor.isClient){
-   Meteor.subscribe('thePlayers');
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+  });
+
+    var loc;
+    var act;
     
-    Template.location.events({
-        'click #KSC': function() {
-            console.log("clicked KSC");
-         //Meetings.find({}, ({ location:"KSC" }))
-        }
-    });
-
-   Template.leaderboard.helpers({
-      'player': function(){
-        var currentUserId = Meteor.userId();
-    	return PlayersList.find({}, ({createdBy: currentUserId},
-                            {sort: {score: -1, name: 1}}))
-
-      },
-      'selectedClass': function(){
-	    var playerId = this._id;
-	    var selectedPlayer = Session.get('selectedPlayer');
-	    if(playerId == selectedPlayer){
-	    // return "selected"
-	    return "active"
-    	}
-	  },
-	  'showSelectedPlayer': function(){
-	    var selectedPlayer = Session.get('selectedPlayer');
-	    return PlayersList.findOne(selectedPlayer)
-		}       
-       
-    });
+    Template.location.events({ 
+        
+        "click .allevents": function(event){
+             Session.set("currentActivity", "");
+            Session.set("currentLocation", "");
+        },
+        
+    "click .locations": function(event){
+    Session.set("currentActivity", "");
+        loc = event.target.innerHTML; 
+        console.log(loc); 
+       Session.set("currentLocation", loc);
+   }, 
+    "click .activities": function(event) {
+      Session.set("currentLocation", "");
+        act = event.target.innerHTML; 
+        console.log(act); 
+       Session.set("currentActivity", act);
+    }});
     
     Template.body.helpers({
-        firstName: function(){
-        var user = Meteor.user(); 
-        if (user) {
-             currentUserId = user.services.google.given_name;
-            return user.services.google.given_name;    
-        } 
-   }
-});
-
-   Template.leaderboard.events({
-   	'click .player': function(){
-	    var playerId = this._id;
-	    Session.set('selectedPlayer', playerId);
-		},
-
-	'click .increment': function(){
-	    var selectedPlayer = Session.get('selectedPlayer');
-	    Meteor.call('modifyPlayerScore', selectedPlayer, 5);
-		},
-
-	'click .decrement': function(){
-	    var selectedPlayer = Session.get('selectedPlayer');
-	     Meteor.call('modifyPlayerScore', selectedPlayer, -5);
-
-	},
-	'click .remove': function(){
-	    var selectedPlayer = Session.get('selectedPlayer');
-	    PlayersList.remove('removePlayerData',selectedPlayer);
-		}
+        
+     name: function(){
+        var user = Meteor.user().username; 
+        return user;   
+        }, 
+              
+        isHidden: function(hide){
+            return Session.equals('hide', hide)
+        }
    });
-
-   Template.addPlayerForm.events({
-   	'submit form': function(event){
-	    event.preventDefault();
-	    var playerNameVar = event.target.playerName.value;
-	    Meteor.call('insertPlayerData', playerNameVar);
-	}
-   })
-////<<<<<<< HEAD:mwitt_am6.js
-//}
-////=======
-   
-/*Eni's code from the meeting app*/
-
-   
-   
    
 // Global variables to store data in MiniMongo and Session
-Meetings = new Mongo.Collection('meetings');
-Session.setDefault("addMeetingFlag", false);
-Session.setDefault("viewMeetingFlag", false);
-var currentUserId = "Grace"; 
-Meteor.subscribe('theMeetings');
+Events = new Mongo.Collection('events');
+Session.setDefault("addEventFlag", false);
+Session.setDefault("viewEventFlag", false);
+Session.setDefault("currentLocation", "");
+Session.setDefault("currentActivity", "");  
+Session.setDefault("hide", false);
+
+Meteor.subscribe('theEvents');
 
 
 /* Deal with the two buttons that show up first in the page.*/
 Template.body.events({
   "click #addButton": function(){
-    Session.set("addMeetingFlag", true);
+    Session.set("addEventFlag", true);
+    Session.set("hide", !Session.get("hide"));
   },
-  /* Switch name of button and show/hide meetings.*/
+
+  /* Switch name of button and show/hide events.*/
   "click #viewButton": function(event){
-    if (event.target.textContent == "View Meetings"){
-      Session.set("viewMeetingFlag", true);
-      event.target.textContent = "Hide Meetings";
+    if (event.target.textContent == "View Events"){
+      Session.set("viewEventFlag", true);
+      event.target.textContent = "Hide Events";
     }
     else {
-      event.target.textContent = "View Meetings";
-      Session.set("viewMeetingFlag", false);
+      event.target.textContent = "View Events";
+      Session.set("viewEventFlag", false);
     }
   }
 }); 
@@ -168,53 +84,60 @@ the HTML page appearance. Because templates are reactive contexts,
 whenever the value of flags is changed somewhere in the code, the code
 in the template gets rerun, changing the HTML page.
 */
-Template.addMeetingForm.helpers({
+Template.addEventForm.helpers({
   
-  addMeeting: function(){
-    return Session.get("addMeetingFlag");
+  addEvent: function(){
+    return Session.get("addEventFlag");
   }
   
 });
 
 Template.viewColumn.helpers({
+    viewEvent: function(){
+    return Session.get("viewEventFlag");
+  },
+     
+    eventsList: function(){ 
+    var curLoc = Session.get("currentLocation");
+    var curAct = Session.get("currentActivity");
+        if (curLoc != "") {
+              return Events.find({location: curLoc});}
+        else if (curAct != "") {
+            return Events.find({activityStr: curAct}); 
+        } else {
+    return Events.find(); }},
     
-  viewMeeting: function(){
-    return Session.get("viewMeetingFlag");
-  },
-  
-  meetingsList: function(){
-    return Meetings.find({});
-  },
-     firstName: function(){
-        var user = Meteor.user(); 
-        if (user) {
-             currentUserId = user.services.google.given_name;
-            return user.services.google.given_name;    
-        } 
-   }
-});
-                      
-
-/* Deal with adding a meeting to the database */
-Template.addMeetingForm.events({
+    
+    
+     // firstName: function(){
+     //    var user = Meteor.user().username; 
+     //    console.log(user);
+     //    return user;   
+     //    } 
+   }  
+    
+);
+    
+/* Deal with adding a event to the database */
+Template.addEventForm.events({
 
     
   "submit form": function(event){
       console.log("submit form");
 
-      var meetingObject = {
-        name: currentUserId,
-        location: event.target.meetingLocation.value,
-        date: event.target.meetingDate.value,
+      var eventObject = {
+        name: Meteor.user().username,
+        location: event.target.eventLocation.value,
+        date: event.target.eventDate.value,
         startTime: event.target.startTime.value,
         endTime: event.target.endTime.value,
-       activity: event.target.meetingActivity.value,
+       activity: event.target.eventActivity.value,
       };
+    
+      Meteor.call('insertEvent', eventObject);
 
-      Meteor.call('insertMeeting', meetingObject);
-
-      Session.set("addMeetingFlag", false); // make the form disappear
-      console.log(meetingObject);
+      Session.set("addEventFlag", false); // make the form disappear
+      console.log(eventObject);
       event.target.reset(); // to clear the fields for next time
     
       // show message in the page using jQuery
@@ -223,5 +146,60 @@ Template.addMeetingForm.events({
     }
 
 })
+
 }
-//>>>>>>> szachare:Bounce.js
+
+if(Meteor.isServer){
+    
+    Events = new Mongo.Collection('events');
+
+/* Helper function used in the publish method to filter undesired items.*/
+function checkExpiration(){
+  // Iterate through all events and check if time has expired
+  var allEvents = Events.find({});
+  var now = new Date();
+  allEvents.forEach(function(item){
+    if (item.endTimeDate < now)
+      Events.update(item, {$set: {hasExpired: true}});
+  })
+  // remove expired entries
+  Events.remove({hasExpired: true})
+}
+
+
+Meteor.publish('theEvents', function(){
+    checkExpiration();
+    return Events.find({hasExpired: false}, {sort: {startTimeDate: 1}})
+});
+
+
+Meteor.methods({
+  insertEvent: function(eventObj){
+    // create two new fields for startTime and endTime, so that they are
+    // Date() objects
+    var startTimeObj = new Date(eventObj.date + " " + eventObj.startTime);
+    var endTimeObj = new Date(eventObj.date + " " + eventObj.endTime);
+    var currentUserId = Meteor.user().username;
+      
+    Events.insert({
+      name: currentUserId,
+      location: eventObj.location,
+      dateStr: eventObj.date,
+      // Use the moment.js library to format time in US time
+      startTimeStr: moment(startTimeObj).format("hh:mm A"),
+      endTimeStr: moment(endTimeObj).format("hh:mm A"),
+      startTimeDate: startTimeObj,
+      endTimeDate: endTimeObj,
+        activityStr: eventObj.activity,
+      hasExpired: false
+    })
+    console.log("new event added in mongodb");
+  },
+    
+     eventsLoc: function(loc){
+        return Events.find({location: loc});
+    }
+    
+})
+
+}
